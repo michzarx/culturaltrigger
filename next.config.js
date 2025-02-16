ea540ffd1d37767487579188180f612f8d82197e
michzarx <michelezheng@paprika.local>
2025-02-17 07:24:36 +0800

    fix: update deployment configuration

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
   basePath: '/culturaltrigger',
   images: {
     unoptimized: true
-  }
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'json',
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
